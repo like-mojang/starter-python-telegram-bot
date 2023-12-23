eeab1c6924b97248ec5cfe002bad5b044796516f
like-mojang <88312026+like-mojang@users.noreply.github.com>
2023-12-24 07:16:20 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,1 @@
-import uvicorn
-import threading
-
-class a(threading.Thread):
-    def __init__(self):
-        super(a, self).__init__()
-    def run(self):
-        import main
-
-d=a()
-d.start()
-
-
 
