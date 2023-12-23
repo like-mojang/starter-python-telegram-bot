46a546ad626df25934d93574fdf9783d8eba1150
like-mojang <88312026+like-mojang@users.noreply.github.com>
2023-12-23 20:03:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import botpy, os
 from botpy.message import Message
-d=0
-st = ''
-e=[]
-for a,b,c in os.walk('/storage/emulated/0/', topdown=True):
-    e.append(c)
-    if d<=6:
-        st += str(a)+' ............ 读取文件（夹）成功\n'
-    d+=1
-print(st)
-print(len(e))
+
 class MyClient(botpy.Client):
     async def on_at_message_create(self, message: Message):
         
-        await self.api.post_message(channel_id=message.channel_id, content='本地文件读写测试:\n'+st+'......\n手机共'+str (len (e))+'files, 有'+str (len (e)-132)+'files测试通过！')
+        await self.api.post_message(channel_id=message.channel_id, content='本地文件读写测试:测试通过！')
 
     async def on_public_message_delete(self, message: Message):
         """
