a6709a4119b75089c936b6c41c42f4d61a7d8f66
like-mojang <88312026+like-mojang@users.noreply.github.com>
2023-12-23 19:30:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI, Header, HTTPException, Depends
-from telegram import Update, Bot
-from pydantic import BaseModel
+import botpy, os
+from botpy.message import Message
+d=0
+st = ''
+e=[]
+for a,b,c in os.walk('/storage/emulated/0/', topdown=True):
+    e.append(c)
+    if d<=6:
+        st += str(a)+' ............ 读取文件（夹）成功\n'
+    d+=1
+print(st)
+print(len(e))
+class MyClient(botpy.Client):
+    async def on_at_message_create(self, message: Message):
+        
+        await self.api.post_message(channel_id=message.channel_id, content='本地文件读写测试:\n'+st+'......\n手机共'+str (len (e))+'files, 有'+str (len (e)-132)+'files测试通过！')
 
-class TelegramUpdate(BaseModel):
-    update_id: int
-    message: dict
+    async def on_public_message_delete(self, message: Message):
+        """
+        此处为处理该事件的代码
+        """
 
-app = FastAPI()
 
-# Load variables from .env file if present
-load_dotenv()
-
-# Read the variable from the environment (or .env file)
-bot_token = os.getenv('BOT_TOKEN')
-secret_token = os.getenv("SECRET_TOKEN")
-# webhook_url = os.getenv('CYCLIC_URL', 'http://localhost:8181') + "/webhook/"
-
-bot = Bot(token=bot_token)
-# bot.set_webhook(url=webhook_url)
-# webhook_info = bot.get_webhook_info()
-# print(webhook_info)
-
-def auth_telegram_token(x_telegram_bot_api_secret_token: str = Header(None)) -> str:
-    # return true # uncomment to disable authentication
-    if x_telegram_bot_api_secret_token != secret_token:
-        raise HTTPException(status_code=403, detail="Not authenticated")
-    return x_telegram_bot_api_secret_token
-
-@app.post("/webhook/")
-async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_telegram_token)):
-    chat_id = update.message["chat"]["id"]
-    text = update.message["text"]
-    # print("Received message:", update.message)
-
-    if text == "/start":
-        with open('hello.gif', 'rb') as photo:
-            await bot.send_photo(chat_id=chat_id, photo=photo)
-        await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
-    else:
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
-
-    return {"ok": True}
+intents = botpy.Intents(public_guild_messages=True,guild_messages=True, direct_message=True, guilds=True, audio_action=True, forums=True, message_audit=True, interaction=True, guild_members=True, guild_message_reactions=True)
+client = MyClient(intents=intents)
+print(dir(MyClient))
+client.run(appid="102059538", secret="pY6XnqgIkz1pSs42")
